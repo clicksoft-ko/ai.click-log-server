8d34e0442e7506712da0224fb45fb351b1cf7e4d
Kaburi <specialemail90@gmail.com>
2024-08-21 16:57:57 +0900

    trust proxy 적용

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { winstonLogger } from './config/winston-logger.config';
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useLogger(winstonLogger);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', true);
   app.setGlobalPrefix("/api");
+  
+  app.useLogger(winstonLogger);
+
   await app.listen(3000);
 }
 bootstrap();
