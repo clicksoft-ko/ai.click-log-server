1a5f17e9a4f3fce837cd7e32289104695d96cbb2
Kaburi <specialemail90@gmail.com>
2024-08-29 12:03:13 +0900

    .

--- src/common/pipes/zod-validation.pipe.ts
+++ src/common/pipes/zod-validation.pipe.ts
@@ -1,4 +1,4 @@
-import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
 import { ZodSchema } from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
