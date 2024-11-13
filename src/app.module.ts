cf6615c5970fb51bb4e4305a6b7aef9153d4cd75
Kaburi <specialemail90@gmail.com>
2024-11-13 15:44:34 +0900

    EnvService global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
-import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { AuthModule } from './api/auth/auth.module';
+import { ClickModule } from './api/click/click.module';
+import { CsModule } from './api/cpm/cs/cs.module';
+import { SurveyModule } from './api/survey/survey.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClickModule } from './api/click/click.module';
 import { globalFilterProviders } from './common/filters/global-filter-providers';
-import { PrismaModule } from './database/prisma/prisma.module';
 import { ConfigModule } from './config/config.module';
-import { SurveyModule } from './api/survey/survey.module';
+import { PrismaModule } from './database/prisma/prisma.module';
 import { MonitoringModule } from './modules/monitoring/monitoring.module';
-import { CsModule } from './api/cpm/cs/cs.module';
-import { AuthModule } from './api/auth/auth.module';
-import { EnvModule } from './modules/env/env.module';
 
 @Module({
-  imports: [ConfigModule, ClickModule, PrismaModule, SurveyModule, MonitoringModule, CsModule, AuthModule, EnvModule],
+  imports: [ConfigModule, ClickModule, PrismaModule, SurveyModule, MonitoringModule, CsModule, AuthModule],
   controllers: [AppController],
   providers: [...globalFilterProviders, AppService, Logger],
 })
