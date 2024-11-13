0b45046c8129a295a2cd089a080ae86e4c59308f
Kaburi <specialemail90@gmail.com>
2024-11-13 15:39:34 +0900

    .

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SigninDto } from './dto/signin.dto';
 import { JwtService } from '@nestjs/jwt';
 import { AuthPayloadDto } from './dto/auth-payload.dto';
+import { EnvService } from '@/modules/env/env.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) { }
+  constructor(private readonly jwtService: JwtService, private readonly envService: EnvService) { }
 
   async signin(dto: SigninDto) {
-    if (dto.userId !== process.env.USER_ID || dto.password !== process.env.PASSWORD) {
+    if (dto.userId !== this.envService.USER_ID || dto.password !== this.envService.PASSWORD) {
       throw new UnauthorizedException();
     }
 
