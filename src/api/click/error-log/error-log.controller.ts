f1e085dda1db9df4788a9aff16eb6a736b746d38
Kaburi <specialemail90@gmail.com>
2024-11-22 19:52:21 +0900

    swagger 수정

--- src/api/click/error-log/error-log.controller.ts
+++ src/api/click/error-log/error-log.controller.ts
@@ -1,15 +1,14 @@
 import { ZodValidate } from '@/common/decorators/zod-validate';
+import { AuthGuard } from '@/common/guards/auth.guard';
 import { ZodValidationPipe } from '@/common/pipes/zod-validation.pipe';
-import { apiHeader } from '@/constants/api-header';
 import { getIp } from '@/shared/utils/ip.util';
 import { Body, Controller, Get, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
-import { ApiHeader, ApiResponse } from '@nestjs/swagger';
+import { ApiResponse } from '@nestjs/swagger';
 import { Request } from 'express';
 import { ErrorLogStacktraceDto } from './dto/error-log-stacktrace.dto';
 import { GetErrorLogQueryDto, GetErrorLogSchema } from './dto/get-error-log.dto';
 import { ErrorLogSchema, SaveErrorLogDto } from './dto/save-error-log.dto';
 import { ErrorLogService } from './error-log.service';
-import { AuthGuard } from '@/common/guards/auth.guard';
 
 @Controller('click/error-log')
 export class ErrorLogController {
@@ -22,11 +21,6 @@ export class ErrorLogController {
     return this.errorLogService.saveErrorLog(dto, ip);
   }
 
-  @ApiHeader({
-    name: apiHeader.click.key,
-    description: 'API를 사용하기 위해서 반드시 필요한 정보',
-    required: true,
-  })
   @UseGuards(AuthGuard)
   @Get()
   @ZodValidate(GetErrorLogSchema)
@@ -34,11 +28,6 @@ export class ErrorLogController {
     return this.errorLogService.getErrorLogs(query);
   }
 
-  @ApiHeader({
-    name: apiHeader.click.key,
-    description: 'API를 사용하기 위해서 반드시 필요한 정보',
-    required: true,  // 헤더가 필수임을 명시
-  })
   @ApiResponse({
     description: 'The stacktrace has been successfully retrieved.',
     type: ErrorLogStacktraceDto,
