04b28d30167d551954b0c4f488554213f30e547f
Kaburi <specialemail90@gmail.com>
2024-11-25 14:03:38 +0900

    에러로그 조회 시 메모리 효율 증가 작업(stream)

--- src/api/click/error-log/error-log.controller.ts
+++ src/api/click/error-log/error-log.controller.ts
@@ -28,43 +28,7 @@ export class ErrorLogController {
   @UseGuards(AuthGuard)
   @Get()
   @ZodValidate(GetErrorLogSchema)
-  async getErrorLogs(@Query(new ZodValidationPipe(GetErrorLogSchema)) query: GetErrorLogQueryDto,
-    @Res() res: Response) {
-    const errorLogs = await this.errorLogService.getErrorLogs(query);
-
-    const stream = new Readable({
-      objectMode: true,
-      read(this: Readable & { started: boolean; index: number }) {
-        if (!this.started) {
-          this.push('[');
-          this.started = true;
-          this.index = 0;
-          return;
-        }
-
-        if (this.index < errorLogs.length) {
-          const prefix = this.index > 0 ? ',' : ''; // 첫 항목이 아니면 쉼표 추가
-          this.push(prefix + JSON.stringify(errorLogs[this.index]));
-          this.index++;
-        } else if (this.index === errorLogs.length) {
-          this.push(']'); // 배열 종료
-          this.push(null);
-          this.index++;
-        }
-      },
-    });
-
-    res.set({
-      'Content-Type': 'application/json',
-      'Transfer-Encoding': 'chunked',
-    });
-
-    stream.pipe(res);
-  }
-
-  @Get('/test')
-  @ZodValidate(GetErrorLogSchema)
-  async getTestErrorLogs(
+  async getErrorLogs(
     @Query(new ZodValidationPipe(GetErrorLogSchema)) query: GetErrorLogQueryDto,
     @Res() res: Response) {
     const errorLogs = await this.errorLogService.getErrorLogs(query);
@@ -87,6 +51,7 @@ export class ErrorLogController {
 
     stream.pipe(res);
   }
+
 
   @ApiResponse({
     description: 'The stacktrace has been successfully retrieved.',
