953cacadc8831aabd3ceae4129ff6e5fe57b7c49
Kaburi <specialemail90@gmail.com>
2024-11-25 13:30:39 +0900

    .

--- src/api/click/error-log/error-log.controller.ts
+++ src/api/click/error-log/error-log.controller.ts
@@ -10,6 +10,7 @@ import { GetErrorLogQueryDto, GetErrorLogSchema } from './dto/get-error-log.dto'
 import { ErrorLogSchema, SaveErrorLogDto } from './dto/save-error-log.dto';
 import { ErrorLogService } from './error-log.service';
 import { Readable } from 'stream';
+import * as JSONStream from 'JSONStream';
 
 @Controller('click/error-log')
 export class ErrorLogController {
@@ -27,35 +28,58 @@ export class ErrorLogController {
   @UseGuards(AuthGuard)
   @Get()
   @ZodValidate(GetErrorLogSchema)
-  getErrorLogs(@Query(new ZodValidationPipe(GetErrorLogSchema)) query: GetErrorLogQueryDto) {
-    return this.errorLogService.getErrorLogs(query);
-  }
-
-  @Get('/test')
-  @ZodValidate(GetErrorLogSchema)
-  async getTestErrorLogs(@Query(new ZodValidationPipe(GetErrorLogSchema)) query: GetErrorLogQueryDto,
+  async getErrorLogs(@Query(new ZodValidationPipe(GetErrorLogSchema)) query: GetErrorLogQueryDto,
     @Res() res: Response) {
     const errorLogs = await this.errorLogService.getErrorLogs(query);
+
+    const stream = new Readable({
+      objectMode: true,
+      read(this: Readable & { started: boolean; index: number }) {
+        if (!this.started) {
+          this.push('[');
+          this.started = true;
+          this.index = 0;
+          return;
+        }
+
+        if (this.index < errorLogs.length) {
+          const prefix = this.index > 0 ? ',' : ''; // 첫 항목이 아니면 쉼표 추가
+          this.push(prefix + JSON.stringify(errorLogs[this.index]));
+          this.index++;
+        } else if (this.index === errorLogs.length) {
+          this.push(']'); // 배열 종료
+          this.push(null);
+          this.index++;
+        }
+      },
+    });
 
     res.set({
       'Content-Type': 'application/json',
       'Transfer-Encoding': 'chunked',
     });
 
-    const stream = new Readable({
-      objectMode: true,
-      read() {
-        // 한 번에 모든 데이터를 스트림으로 전송
-        this.push('[');
-        errorLogs.forEach((log, index) => {
-          this.push(`${index > 0 ? ',' : ''}${JSON.stringify(log)}`);
-        });
-        this.push(']');
-        this.push(null);
-      }
-    });
-
     stream.pipe(res);
+  }
+
+  @Get('/test')
+  @ZodValidate(GetErrorLogSchema)
+  async getTestErrorLogs(
+    @Query(new ZodValidationPipe(GetErrorLogSchema)) query: GetErrorLogQueryDto,
+    @Res() res: Response
+  ) {
+    const errorLogs = await this.errorLogService.getErrorLogs(query);
+    
+    res.set({
+      'Content-Type': 'application/json',
+      'Transfer-Encoding': 'chunked',
+    });
+  
+    const jsonStream = JSONStream.stringify();
+    jsonStream.pipe(res);
+    
+    errorLogs.forEach(log => jsonStream.write(log));
+    jsonStream.end();
   }
 
   @ApiResponse({
