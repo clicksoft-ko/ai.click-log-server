ea5c35fac7d2955452f2a1ceb6e63211908b24a9
Kaburi <specialemail90@gmail.com>
2025-03-26 11:56:05 +0900

    .

--- src/shared/utils/ip.util.ts
+++ src/shared/utils/ip.util.ts
@@ -20,20 +20,5 @@ export function getIp(req: Request): string | undefined {
 }
 
 export function getIp2(req: Request) {
-  const xOriginForwardedFor = req.headers['x-original-forwarded-for'] as string;
-  if (xOriginForwardedFor) {
-    return xOriginForwardedFor.split(":")[0];
-  }
-
-  const xForwardedFor = req.headers['x-forwarded-for'] as string;
-  if (xForwardedFor) {
-    return xForwardedFor.split(',')[0].trim()
-  }
-
-  const realIp = req.headers['x-real-ip'] as string;
-  if (realIp) {
-    return realIp;
-  }
-
-  return { headers: req.headers, test: "test" };
+  return req.headers;
 }
